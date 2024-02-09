76be9e73b29f2e0f5b5dffdfd4166c852c3822eb
Alexey Karnachev <alekseykarnachev@gmail.com>
2024-02-10 03:10:10 +0400

    basic player movement

--- bin/texor.c
+++ bin/texor.c
@@ -16,6 +16,7 @@ typedef struct World {
 static World WORLD;
 
 static World init_world();
+static void update_world(World *WORLD);
 
 int main(void) {
     SetConfigFlags(FLAG_MSAA_4X_HINT);
@@ -25,9 +26,12 @@ int main(void) {
     WORLD = init_world();
 
     while (!WindowShouldClose()) {
+        update_world(&WORLD);
+
         BeginDrawing();
 
         BeginMode3D(WORLD.camera);
+        ClearBackground(BLANK);
         DrawSphere(WORLD.player.transform.translation, 1.0, RED);
         EndMode3D();
 
@@ -38,15 +42,42 @@ int main(void) {
 static World init_world(void) {
     World world = {0};
 
+    // -------------------------------------------------------------------
+    // Player
     world.player.transform.rotation = QuaternionIdentity();
     world.player.transform.scale = Vector3One();
     world.player.transform.translation = Vector3Zero();
 
+    // -------------------------------------------------------------------
+    // Camera
+    Vector3 player_position = world.player.transform.translation;
     world.camera.fovy = 40.0;
-    world.camera.position = (Vector3){0.0, 0.0, 50.0};
-    world.camera.target = Vector3Zero();
+    world.camera.position = player_position;
+    world.camera.position.z = 50.0;
+    world.camera.target = player_position;
     world.camera.up = (Vector3){0.0, 1.0, 0.0};
     world.camera.projection = CAMERA_PERSPECTIVE;
 
     return world;
+}
+
+
+static void update_world(World *world) {
+    float dt = GetFrameTime();
+
+    // -------------------------------------------------------------------
+    // Player
+    Vector2 dir = Vector2Zero();
+    dir.y += IsKeyDown(KEY_W);
+    dir.y -= IsKeyDown(KEY_S);
+    dir.x -= IsKeyDown(KEY_A);
+    dir.x += IsKeyDown(KEY_D);
+    if (Vector2Length(dir) > EPSILON) {
+        Vector2 step = Vector2Scale(
+            Vector2Normalize(dir),
+            20.0 * dt
+        );
+        world->player.transform.translation.x += step.x;
+        world->player.transform.translation.y += step.y;
+    }
 }
